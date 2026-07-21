8454866e831b39f755a4faf1c12188f16712aa8c
imyash0722 <imyash0722@users.noreply.github.com>
2026-07-21 16:36:01 +0530

    Update reload script to restart swww

--- .config/niri/scripts/reload.sh
+++ .config/niri/scripts/reload.sh
@@ -5,4 +5,8 @@ pkill -x waybar
 nohup waybar -c ~/.config/niri/waybar/config -s ~/.config/niri/waybar/style.css >/dev/null 2>&1 &
 
 # Reload Wallpaper
+pkill -x swww-daemon
+sleep 1
+swww-daemon &
+sleep 1
 swww img ~/Wallpapers/4000x4000__Hakui_By-pass-ezgif.com-crop.gif
