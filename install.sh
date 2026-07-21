502fcbda9476263e450b645cd067336643915d79
imyash0722 <imyash0722@users.noreply.github.com>
2026-07-21 20:07:06 +0530

    Add xcb-util-cursor to fix Qt crashes on Wayland

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ echo "=========================================="
 
 echo "[1/4] Installing required dependencies..."
 # This command requires paru (AUR helper) to be installed
-paru -S --needed --noconfirm ungoogled-chromium-bin dolphin konsole typora-free-with-plugin vesktop niri waybar rofi-wayland foot fastfetch ly networkmanager bluetui pipemixer satty btop neovim zsh firefox-developer-edition awww hyprlock grim slurp wl-clipboard cliphist cava starship swaync hypridle ffmpeg jq brightnessctl playerctl rofi-rbw wtype obs-studio imagemagick kdeconnect kwallet kanshi ttf-jetbrains-mono ttf-roboto eza bat fzf zoxide ripgrep fd ttf-font-awesome ttf-meslo-nerd breeze breeze-icons plasma-integration power-profiles-daemon iwd iwgtk lazygit yazi tealdeer qimgv haruna ark okular bongocat libreoffice-still zapzap
+paru -S --needed --noconfirm ungoogled-chromium-bin dolphin konsole typora-free-with-plugin vesktop niri waybar rofi-wayland foot fastfetch ly networkmanager bluetui pipemixer satty btop neovim zsh firefox-developer-edition awww hyprlock grim slurp wl-clipboard cliphist cava starship swaync hypridle ffmpeg jq brightnessctl playerctl rofi-rbw wtype obs-studio imagemagick kdeconnect kwallet kanshi ttf-jetbrains-mono ttf-roboto eza bat fzf zoxide ripgrep fd ttf-font-awesome ttf-meslo-nerd breeze breeze-icons plasma-integration power-profiles-daemon iwd iwgtk lazygit yazi tealdeer qimgv haruna ark okular bongocat libreoffice-still zapzap xcb-util-cursor
 
 echo "[2/4] Copying config files..."
 mkdir -p ~/.config
