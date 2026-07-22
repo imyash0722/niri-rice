143b373d322c72e2a85da26842786ae622772cc1
imyash0722 <imyash0722@users.noreply.github.com>
2026-07-22 11:05:25 +0530

    Remove tailscale from installer

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ echo "=========================================="
 
 echo "[1/4] Installing required dependencies..."
 # This command requires paru (AUR helper) to be installed
-paru -S --needed --noconfirm ungoogled-chromium-bin dolphin konsole typora-free-with-plugin vesktop niri waybar rofi-wayland foot fastfetch ly networkmanager bluetui pipemixer satty btop neovim zsh firefox-developer-edition awww hyprlock grim slurp wl-clipboard cliphist cava starship mako hypridle ffmpeg jq brightnessctl playerctl rofi-rbw wtype obs-studio imagemagick kwallet kanshi ttf-jetbrains-mono ttf-roboto eza bat batctl-tui fzf zoxide ripgrep fd ttf-font-awesome ttf-meslo-nerd breeze breeze-icons plasma-integration power-profiles-daemon iwd iwgtk lazygit yazi tealdeer qimgv haruna ark okular bongocat libreoffice-still zapzap xcb-util-cursor tailscale
+paru -S --needed --noconfirm ungoogled-chromium-bin dolphin konsole typora-free-with-plugin vesktop niri waybar rofi-wayland foot fastfetch ly networkmanager bluetui pipemixer satty btop neovim zsh firefox-developer-edition awww hyprlock grim slurp wl-clipboard cliphist cava starship mako hypridle ffmpeg jq brightnessctl playerctl rofi-rbw wtype obs-studio imagemagick kwallet kanshi ttf-jetbrains-mono ttf-roboto eza bat batctl-tui fzf zoxide ripgrep fd ttf-font-awesome ttf-meslo-nerd breeze breeze-icons plasma-integration power-profiles-daemon iwd iwgtk lazygit yazi tealdeer qimgv haruna ark okular bongocat libreoffice-still zapzap xcb-util-cursor
 
 echo "[2/4] Copying config files..."
 mkdir -p ~/.config
@@ -37,7 +37,7 @@ chmod +x ~/.config/hypr/scripts/*.sh
 
 echo "[5/5] Enabling system services..."
 sudo systemctl enable --now power-profiles-daemon || true
-sudo systemctl enable --now tailscaled || true
+
 
 echo "=========================================="
 echo " Installation Complete!"
