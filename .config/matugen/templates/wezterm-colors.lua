a67aaa0e8e3a42f237482000de8834ac070382c3
imyash0722 <imyash0722@users.noreply.github.com>
2026-09-15 09:46:55 +0530

    feat(wezterm): add Matugen dynamic theme generator and WezTerm config parity with Kitty

---
+++ .config/matugen/templates/wezterm-colors.lua
@@ -0,0 +1,56 @@
+-- Auto-generated Material You color palette for WezTerm by Matugen
+return {
+  foreground = '#{{ colors.on_surface.default.hex | replace: "#", "" }}',
+  background = '#{{ colors.background.default.hex | replace: "#", "" }}',
+
+  cursor_bg = '#{{ colors.primary.default.hex | replace: "#", "" }}',
+  cursor_fg = '#{{ colors.on_primary.default.hex | replace: "#", "" }}',
+  cursor_border = '#{{ colors.primary.default.hex | replace: "#", "" }}',
+
+  selection_fg = '#{{ colors.on_secondary_container.default.hex | replace: "#", "" }}',
+  selection_bg = '#{{ colors.secondary_container.default.hex | replace: "#", "" }}',
+
+  scrollbar_thumb = '#{{ colors.surface_container.default.hex | replace: "#", "" }}',
+  split = '#{{ colors.outline.default.hex | replace: "#", "" }}',
+
+  ansi = {
+    '#{{ colors.background.default.hex | replace: "#", "" }}',
+    '#F7768E',
+    '#9ECE6A',
+    '#E0AF68',
+    '#7AA2F7',
+    '#{{ colors.primary.default.hex | replace: "#", "" }}',
+    '#7DCFFF',
+    '#{{ colors.on_surface.default.hex | replace: "#", "" }}',
+  },
+  brights = {
+    '#{{ colors.outline.default.hex | replace: "#", "" }}',
+    '#FF7A93',
+    '#B9F27C',
+    '#FF9E64',
+    '#89B4FA',
+    '#{{ colors.tertiary.default.hex | replace: "#", "" }}',
+    '#B4F9F8',
+    '#FFFFFF',
+  },
+
+  tab_bar = {
+    background = '#{{ colors.background.default.hex | replace: "#", "" }}',
+    active_tab = {
+      bg_color = '#{{ colors.primary.default.hex | replace: "#", "" }}',
+      fg_color = '#{{ colors.on_primary.default.hex | replace: "#", "" }}',
+    },
+    inactive_tab = {
+      bg_color = '#{{ colors.surface_container.default.hex | replace: "#", "" }}',
+      fg_color = '#{{ colors.on_surface_variant.default.hex | replace: "#", "" }}',
+    },
+    inactive_tab_hover = {
+      bg_color = '#{{ colors.surface_container_highest.default.hex | replace: "#", "" }}',
+      fg_color = '#{{ colors.on_surface.default.hex | replace: "#", "" }}',
+    },
+    new_tab = {
+      bg_color = '#{{ colors.surface_container.default.hex | replace: "#", "" }}',
+      fg_color = '#{{ colors.on_surface_variant.default.hex | replace: "#", "" }}',
+    },
+  },
+}
---
+++ .config/wezterm/colors.lua
@@ -0,0 +1,56 @@
+-- Auto-generated Material You color palette for WezTerm by Matugen
+return {
+  foreground = '#e7e0e8',
+  background = '#151218',
+
+  cursor_bg = '#d7bafb',
+  cursor_fg = '#3c255a',
+  cursor_border = '#d7bafb',
+
+  selection_fg = '#ebddf7',
+  selection_bg = '#4c4357',
+
+  scrollbar_thumb = '#211e24',
+  split = '#958e99',
+
+  ansi = {
+    '#151218',
+    '#F7768E',
+    '#9ECE6A',
+    '#E0AF68',
+    '#7AA2F7',
+    '#d7bafb',
+    '#7DCFFF',
+    '#e7e0e8',
+  },
+  brights = {
+    '#958e99',
+    '#FF7A93',
+    '#B9F27C',
+    '#FF9E64',
+    '#89B4FA',
+    '#f2b7c1',
+    '#B4F9F8',
+    '#FFFFFF',
+  },
+
+  tab_bar = {
+    background = '#151218',
+    active_tab = {
+      bg_color = '#d7bafb',
+      fg_color = '#3c255a',
+    },
+    inactive_tab = {
+      bg_color = '#211e24',
+      fg_color = '#ccc4cf',
+    },
+    inactive_tab_hover = {
+      bg_color = '#37333a',
+      fg_color = '#e7e0e8',
+    },
+    new_tab = {
+      bg_color = '#211e24',
+      fg_color = '#ccc4cf',
+    },
+  },
+}
